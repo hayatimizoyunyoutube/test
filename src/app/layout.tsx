56f11393fc300a591a80709bd3f8fbd042bb6a72
hayatimizoyunyoutube <mertdundaroyunda@gmail.com>
2026-05-30 13:09:35 +0000

    v0.0.1 src app route 404 fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,9 +8,9 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="tr">
       <body>{children}</body>
